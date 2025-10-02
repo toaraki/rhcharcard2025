657a30f837f5ed5c2364a6cea8711e790b82bf14
Toshihiro Araki <toaraki@redhat.com>
2025-10-02 21:40:53 +0900

    Update image select logic

--- setup.sh
+++ setup.sh
@@ -15,8 +15,12 @@ npm install
 # 環境変数を設定 (ホスト名とメダル情報を渡す)
 # cloud-initのuser-dataでこれらの変数を設定してください
 HOST_NAME=$(hostname)
-#MEDAL_IMAGE_URL="https://your-server.com/public/medal_$(shuf -i 1-4 -n 1).png"
-MEDAL_IMAGE_URL="/medal_$(shuf -i 1-7 -n 1).png"
+# MEDAL_IMAGE_URL="https://your-server.com/public/medal_$(shuf -i 1-4 -n 1).png"
+# MEDAL_IMAGE_URL="/medal_$(shuf -i 1-7 -n 1).png"
+MEDAL_IMAGE_URL="https://your-server.com/public/medal_random.png"
+curl -o "/opt/app/medal_image.png" "$MEDAL_IMAGE_URL"
+# 画像の参照パスをローカルのファイルに変更
+MEDAL_IMAGE_URL="/medal_image.png"
 
 # VMスペック情報の取得
 CPU_CORES=$(nproc)
